cbe0a52d9d45b37e266eaa4b0a0ef173414561ae
Daniil Talashkevich <talashkevich.da@phystech.edu>
2021-03-03 12:23:51 +0300

    Add 2nd homework for FPGA

--- homework2/cnthex.v
+++ homework2/cnthex.v
@@ -2,7 +2,7 @@ module cnthex( input wire clk, output reg [15:0]hex);
 
 always @(posedge clk)
 begin
-	hex <= (hex === 16'hfff) ? 16'h0 : hex + 16'h1;
+	hex <= (hex === 16'hffff) ? 16'h0 : hex + 16'h1;
 end
 
 endmodule
