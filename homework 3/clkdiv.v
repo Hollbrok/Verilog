b2023e4bdef46db5b3e3b3d192d8c81cb539b09e
Daniil Talashkevich <talashkevich.da@phystech.edu>
2021-03-26 13:54:44 +0300

    fix 3 homework

--- homework 3/clkdiv.v
+++ homework 3/clkdiv.v
@@ -1,16 +1,15 @@
-module clk_div #(parameter X = 12)
+module clkdiv #(parameter X = 21)
 (
-	input clkIn,
-
-	output clkOut
+	input wire clk, 
+	output wire clkdiv
 );
 
-reg [X - 1 : 0]cnt = 0;
+reg [X - 1:0]cnt = 0;
+assign clkdiv = cnt[X - 1];
 
-assign clkOut = cnt[X - 1];
-
-always @(posedge clkIn) begin
-    cnt <= cnt + 1;
+always @(posedge clk)
+begin
+	cnt <= (cnt === {X{1'b1}}) ? 0 : cnt + 1;
 end
 
 endmodule
--- homework 3/top.v
+++ homework 3/top.v
@@ -1,41 +1,46 @@
 module top(
     input CLK,
-
-    output DS_EN1, DS_EN2, DS_EN3, DS_EN4,
-    output DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G
+	 output DS_EN1, DS_EN2, DS_EN3, DS_EN4,
+	 output DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G
 );
 
-wire [15:0]d;
+//assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = 4'b0011;
 
-wire [3:0]anodes;
-assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = ~anodes;
+wire clkdiv1;
+wire clkdiv10;
 
-wire [6:0]segments;
-assign {DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G} = segments;
+wire [15:0]grey_num;
+reg [3:0]grey_address = 0;
 
-clk_div clk_div(.clk_in(CLK), .clk_out(clk2));
+wire [6:0]seg_1;
+wire [6:0]seg_2;
+wire [6:0]seg_3;
+wire [6:0]seg_4;
 
-clk_div #(.X(24)) clk_div2(.clk_in(CLK), .clk_out(clk3));
+clkdiv #(.X(24)) clkdiv_counter( .clk(CLK), .clkdiv(clkdiv1));
+//cnthex Cnthex1( .clk(clkdiv1), .hex(hex));
 
-hex_display hd(.clk(clk2), .data(d), .anodes(anodes), .segments(segments));
+clkdiv #(.X(10)) Clkdiv_update( .clk(CLK), .clkdiv(clkdiv10));
 
-rom_fetcher rom_fetcher(.clk(clk3), .q(d));
-
-endmodule
-
-module rom_fetcher(
-    input clk,
-
-    output [15:0]q
-);
-
-reg [7:0]pc = 0;
-wire [7:0]pc_next = pc + 1;
-
-rom #(.WIDTH(16)) rom(.clk(clk), .addr(pc), .q(q));
-
-always @(posedge clk) begin
-    pc <= pc_next;
+always @(posedge clkdiv1) begin                   
+	grey_address <= grey_address + 1;      
 end
+	 
+rom grey_data(.clock(clkdiv1), .address(grey_address), .q(grey_num));
+
+
+hex2seg Hex2seg1( .hex(grey_num[3:0]),   .seg(seg_1));
+hex2seg Hex2seg2( .hex(grey_num[7:4]),   .seg(seg_2));
+hex2seg Hex2seg3( .hex(grey_num[11:8]),  .seg(seg_3));
+hex2seg Hex2seg4( .hex(grey_num[15:12]), .seg(seg_4));
+
+wire [6:0]seg;
+wire [3:0]segen;
+
+shownumber Shownumber1( .clk10(clkdiv10), .seg1(seg_1), .seg2(seg_2), .seg3(seg_3), .seg4(seg_4), .seg(seg), .segen(segen));
+
+assign {DS_EN1, DS_EN2, DS_EN3, DS_EN4} = segen;
+assign {DS_A, DS_B, DS_C, DS_D, DS_E, DS_F, DS_G} = seg;
+
 
 endmodule
